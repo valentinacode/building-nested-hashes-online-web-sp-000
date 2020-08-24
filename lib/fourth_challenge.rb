291f1e96f5d7d68175f1df10befe2b84e965b917
Valentina E <tiyahelfrih@gmail.com>
2020-08-24 13:11:17 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -3,7 +3,7 @@ def fourth_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero => {name: "Romeo", age: "15", status: "dead"},
       :hero_friends => [
         #your hashes here!
       ]
@@ -11,7 +11,7 @@ def fourth_challenge
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine => {name: "Juliet", age: "15", status: "dead"},
       :heroine_friends => [
         #your hashes here!
       ]
